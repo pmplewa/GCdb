a1061c0868c8f778b47da8c67b1b531c42a529a3
Philipp Plewa <pmplewa@users.noreply.github.com>
2017-04-22 22:42:37 +0200

    Use ndjson data format

--- data/import.sh
+++ data/import.sh
@@ -1,3 +1,3 @@
 mongo test --eval "db.dropDatabase();"
-mongoimport --db test --collection stars --drop --jsonArray --file stars.json
+mongoimport --db test --collection stars --drop --file stars.ndjson
 mongo test --eval "db.stars.createIndex({ position: '2d' }, { min: -100, max: 100 });"
